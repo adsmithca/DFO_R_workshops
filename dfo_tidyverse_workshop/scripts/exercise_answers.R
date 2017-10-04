1aed060bb3c08f756cf504c8cc49e46e6b13915c
KeithLewis <keithl@mun.ca>
2017-10-03 22:20:18 -0230

    Answers have been completed.  Need to double check.

--- dfo_tidyverse_workshop/scripts/exercise_answers.R
+++ dfo_tidyverse_workshop/scripts/exercise_answers.R
@@ -12,6 +12,8 @@
 #3. Identify any issues loading with loading this data
 
 library(readr)
+library(tidyr)
+library(dplyr)
 trawl_abiotic = read_csv("data/trawl_abiotic.csv")
 
 trawl_biomass = read_csv("data/trawl_biomass.csv")
@@ -22,29 +24,65 @@ trawl_biomass = read_csv("data/trawl_biomass.csv")
 #1. Increasing the number of columns read in
 #2. Specifying depth as a "double" instead of an integer
 
-trawl_abiotic = read_csv("data/trawl_abiotic.csv",col_types = cols(depth="d"))
+trawl_abiotic = read_csv("data/trawl_abiotic.csv",guess_max=5000, col_types = cols(depth="d"))
 
 # tidyr
-  #Try spreading the trawl_abiotic data (may want to subset or filter rows and reduce # of columns) 
- #Try gathering the trawl_biomass
-                  
+
+#Try spreading the trawl_abiotic data 
+spread(data = trawl_abiotic, key = nafo_div, value = depth) 
+# note that the next line of code is equivalent to the one above.  It takes more effort but can be easier to read the one above when you haven't looked at the code in a while,
+spread(trawl_abiotic, nafo_div, depth) 
+
+#Try gathering the trawl_biomass
+gather(trawl_biomass, key = species, value = biomass, shrimp:redfish)
+
+
 # pipes
 # Try using pipes to first subset the trawl_abiotic data and then spread
-# Try using pipes to first subset the trawl_biomass before gathering
+# Note that we've snuck a little bit of dplyr in here, i.e., the filter() and select() functions
+trawl_abiotic %>%
+  filter(temp_bottom >3) %>%
+    spread(nafo_div, depth)
 
+# Try using pipes to first subset the trawl_biomass before gathering
+trawl_biomass %>%
+  select(-shrimp, - redfish) %>%
+  gather(species, biomass, cod:halibut)
 
 # dplyr
 ### using the trawl survey data:
 # 1. join the environmental data to biomass data set
+trawl_all <- left_join(trawl_abiotic, trawl_biomass, "trawl_id")
+
 # 2. create a new tibble that only includes the year, species name, and bottom temperature
+subset1 <- select(trawl_all, year.x, shrimp:redfish, temp_bottom)
+
 # 3. create a second tibble that excludes longitude, latitude, and depth
+subset2 <- select(trawl_all, - long, -lat, -depth)
 
 # dplyr
 ### using the joined the data set you created above:
 # 1. Create a new tibble that only includes trawls collected in NAFO division # 2J,  and are deeper than 400 meters
+
+trawl_all %>% 
+  filter(nafo_div == "2J" & depth > 400)
+
 # 2. Create a new tibble, grouped by species and strata, that only includes observations taken after 2000 and have a maximum biomass greater than 1 kg.
 
+trawl_all %>%
+  gather(key = species, value = biomass, shrimp:redfish) %>%
+  group_by(species, strata) %>%
+  filter(year.x >2000 & biomass > 1)
 
 #Try: 
 # - counting the number of observations in each year in each nafo division 
+
+trawl_abiotic %>%
+  group_by(nafo_div, year) %>%
+  summarise(count = n())
+
 # - going to the cheat sheet and pick 1-2 functions for summarising or mutating the data in the manner of your choice
+
+trawl_all %>%
+  gather(key = species, value = biomass, shrimp:redfish) %>%
+  mutate(logbiomass = log(biomass))
