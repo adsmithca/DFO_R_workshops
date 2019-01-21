946c07035755d655d50cef6b5581cba8bf34d57b
Eric Pedersen <eric.pedersen@wisc.edu>
2019-01-21 10:58:12 -0330

    tweaked Rmarkdown to match changes during the workshop

--- dfo_oceans_workshop/scripts/R_working_with_data.R
+++ dfo_oceans_workshop/scripts/R_working_with_data.R
@@ -39,14 +39,17 @@ print(legal_size_plot)
 #data frame, creates the new `LEGAL_SIZE` column, and then use the `filter`
 #command to exclude all of the lobsters than are less than legal size.
 
-legal_size_tags = tidy_tags %>%
-    mutate(LEGAL_SIZE = LENGTH>82.5)%>%
-    filter(LEGAL_SIZE == TRUE)
+legal_size_tags <- tidy_tags %>%
+    mutate(LEGAL_SIZE = LENGTH > 82.5) %>%
+    filter(LEGAL_SIZE == TRUE) 
+
+legal_size_tags <- tidy_tags %>%
+    mutate(LEGAL_SIZE = LENGTH > 82.5) %>%
+    filter(LENGTH > 82.5) 
+
+legal_size_tags
 
 
-legal_size_tags = tidy_tags %>%
-    mutate(LEGAL_SIZE = LENGTH>82.5)%>%
-    filter(LENGTH > 82.5)
 #`group_by()`: breaking data into groups ####
 
 tidy_tags <- tidy_tags %>%
@@ -63,16 +66,16 @@ tidy_tags
 
 #`group_by() is not that useful alone, but is incredibly powerful when combined with other commands: 
 tidy_tags <- tidy_tags %>%
-    group_by(SIDE, ISLAND,SEX) %>%
+    group_by(SIDE, ISLAND, SEX) %>%
     mutate(LENGTH_AVG = mean(LENGTH))%>%
     #It's always important to use ungroup after doing the grouped action
     ungroup()
 
 #Now we can plot this:
-legal_size_plot <- ggplot(tidy_tags, aes(x=YEAR, y= LENGTH, color = SEX))+
+legal_size_plot <- ggplot(tidy_tags, aes(x = YEAR, y = LENGTH, color = SEX))+
     facet_grid(ISLAND~SIDE)+
     geom_point()+
-    geom_hline(aes(yintercept = LENGTH_AVG,color=SEX),size=2)
+    geom_hline(aes(yintercept = LENGTH_AVG, color = SEX), size=2)
 
 print(legal_size_plot)
     
@@ -81,7 +84,7 @@ print(legal_size_plot)
 # Modify the following code so that you are calculating the length of time that 
 # each tag value has been observed for each sex and each island (remember to use ungroup at the end!)
 
-legal_tags <- tidy_tags %>%
+legal_size_tags <- tidy_tags %>%
     mutate(LEGAL_SIZE = LENGTH > 82.5,
            SIDE = tolower(SIDE)) %>%
     filter(LEGAL_SIZE == TRUE) %>%
@@ -90,7 +93,7 @@ legal_tags <- tidy_tags %>%
 
 
 # Now we can (almost) create the first plot we saw:
-legal_tags <- tidy_tags %>%
+legal_size_tags <- tidy_tags %>%
     mutate(LEGAL_SIZE = LENGTH > 82.5,
            SIDE = tolower(SIDE)) %>%
     filter(LEGAL_SIZE == TRUE) %>%
@@ -99,12 +102,12 @@ legal_tags <- tidy_tags %>%
     filter(YEARS_OBS >0)%>%
     ungroup()
 
-longest_observed_lobsters <- legal_tags %>%
+longest_observed_lobsters <- legal_size_tags %>%
     group_by(SEX,ISLAND)%>%
     filter(YEARS_OBS == max(YEARS_OBS))%>%
     ungroup()
 
-size_plot <- legal_tags %>%
+size_plot <- legal_size_tags %>%
     ggplot(aes(x = YEAR, y = LENGTH, color = SEX))+
     facet_grid(SEX~ISLAND)+
     geom_line(aes(group = TAG),alpha=0.25)+
@@ -117,7 +120,7 @@ print(size_plot)
 # Using summerize() ####
 
 average_lengths <- tidy_tags %>%
-    group_by(ISLAND,SIDE ,cut(LENGTH,breaks = c(80,90,100))) %>%
+    group_by(ISLAND,SIDE ) %>%
     summarize(LENGTH_AVG = mean(LENGTH),
               LENGTH_MIN = min(LENGTH),
               LENGTH_MAX = max(LENGTH))%>%
@@ -126,7 +129,7 @@ average_lengths <- tidy_tags %>%
 
 average_lengths
 
-lobster_growth_rates = legal_tags %>%
+lobster_growth_rates = legal_size_tags %>%
     group_by(TAG, SEX, ISLAND)%>%
     filter(max(YEAR)>min(YEAR))%>%
     summarize(LENGTH_START = min(LENGTH),
@@ -147,7 +150,7 @@ growth_plot
 # This is the full code to generate the clean versions of the figures we showed at the start ####
 
 #This shows how to make the year-versus length plot
-legal_tags <- tidy_tags %>%
+legal_size_tags <- tidy_tags %>%
     mutate(LEGAL_SIZE = LENGTH > 82.5,
            SIDE = tolower(SIDE)) %>%
     filter(LEGAL_SIZE == TRUE) %>%
@@ -156,12 +159,12 @@ legal_tags <- tidy_tags %>%
     filter(YEARS_OBS >0)%>%
     ungroup()
 
-longest_observed_lobsters <- legal_tags %>%
+longest_observed_lobsters <- legal_size_tags %>%
     group_by(SEX,ISLAND)%>%
     filter(YEARS_OBS == max(YEARS_OBS))%>%
     ungroup()
 
-size_plot <- legal_tags %>%
+size_plot <- legal_size_tags %>%
     ggplot(aes(x = YEAR, y = LENGTH, color = SEX))+
     facet_grid(SEX~ISLAND)+
     geom_line(aes(group = TAG),alpha=0.25)+
@@ -179,7 +182,7 @@ print(size_plot)
 
     
 #This shows how to make the growth-by-area plot:
-lobster_growth_rates = legal_tags %>%
+lobster_growth_rates = legal_size_tags %>%
     group_by(TAG, SEX, ISLAND)%>%
     filter(max(YEAR)>min(YEAR))%>%
     summarize(LENGTH_START = min(LENGTH),
